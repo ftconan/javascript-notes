aceccdf1f1ae04a9d73805d871aa7e9d8574d8a7
ftconan <1508907793@qq.com>
2020-01-14 23:17:39 +0800

    alert_demo.js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# javascript
+# JavaScript
 JavaScript study notes
+## 廖雪峰JavaScript学习笔记
+1. JavaScript教程
---
+++ js_notes/js_introduce/alert_demo.js
@@ -0,0 +1,9 @@
+/*
+@author: magician
+@file:   alert_demo
+@date:   2020/1/14
+*/
+
+// ReferenceError: alert is not defined(alert is not js grammar)
+// alert('hello');
+console.log('11');
