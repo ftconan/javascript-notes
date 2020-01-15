418b212ed29785b1db5cd18df53044c1b8697637
ftconan <1508907793@qq.com>
2020-01-15 13:54:50 +0800

    add var_demo

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ JavaScript study notes
 ## 廖雪峰JavaScript学习笔记
 1. JavaScript教程
 2. 快速入门
+ * 基本语法
---
+++ js_notes/js_start/var_demo.js
@@ -0,0 +1,19 @@
+/*
+@author: magician
+@file:   var_demo
+@date:   2020/1/15
+*/
+if (2 > 1) {
+    x = 1;
+    y = 2;
+    z = 3;
+    if (x < y) {
+        z = 4;
+    }
+    if (x > y) {
+        z = 5;
+    }
+}
+
+console.log(z);
+// alert(z);
