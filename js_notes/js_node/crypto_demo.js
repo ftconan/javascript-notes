9a16ac6f43844ea9b0b30ad56c2a1de9548cc4fa
ftconan <1508907793@qq.com>
2020-02-10 13:46:11 +0800

    add crypto_demo

---
+++ js_notes/js_node/crypto_demo.js
@@ -0,0 +1,6 @@
+/*
+@author: magician
+@file:   crypto_demo.js
+@date:   2020/2/10
+*/
+'use strict';
---
+++ js_notes/js_node/http_demo.js
@@ -0,0 +1,6 @@
+/*
+@author: magician
+@file:   http_demo.js
+@date:   2020/2/10
+*/
+'use strict';
