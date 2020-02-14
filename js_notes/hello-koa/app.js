c27bb1b1c7e3fd3518f941c72cb64503b283c81d
ftconan <1508907793@qq.com>
2020-02-14 14:17:55 +0800

    add mvc

--- js_notes/hello-koa/app.js
+++ js_notes/hello-koa/app.js
@@ -5,67 +5,47 @@
 */
 'use strict';
 
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-// body parse
+
 const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
+const templating = require('./templating');
 
 // 创建一个Koa对象表示web app本身:
 const app = new Koa();
-app.use(bodyParser());
 
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
+const isProduction = process.env.NODE_ENV === 'production';
 
-// 导入controller middleware:
-const controller = require('./controller');
-
-// 使用middleware:
-app.use(controller());
-
-// 模板引擎
-const nunjucks = require('nunjucks');
-
-function createEnv(path, opts) {
+// log request URL
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     let
-        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
-        noCache = opts.noCache || false,
-        watch = opts.watch || false,
-        throwOnUndefined = opts.throwOnUndefined || false,
-        env = new nunjucks.Environment(
-            new nunjucks.FileSystemLoader('views', {
-                noCache: noCache,
-                watch: watch,
-            }), {
-                autoescape: autoescape,
-                throwOnUndefined: throwOnUndefined
-            });
-
-    if (opts.filters) {
-        for (let f in opts.filters) {
-            env.addFilter(f, opts.filters[f]);
-        }
-    }
-
-    return env;
-}
-
-let env = createEnv('views', {
-    watch: true,
-    filters: {
-        hex: function (n) {
-            return '0x' + n.toString(16);
-        }
-    }
+        start = new Date().getTime(),
+        execTime;
+    await next();
+    execTime = new Date().getTime() - start;
+    ctx.response.set('X-Response-Time', `${execTime}ms`);
 });
 
-let s = env.render('hello.html', {name: '小明', fruits: ['apple', 'pear', 'banana']});
-console.log(s);
+// static file support:
+if (!isProduction) {
+    let staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
 
-console.log(env.render('extend.html', {
-    header: 'Hello',
-    body: 'bla bla bla...'
+// parse request body:
+app.use(bodyParser());
+
+// add nunjucks as view:
+app.use(templating('views', {
+    noCache: !isProduction,
+    watch: !isProduction
 }));
+
+// add controller:
+app.use(controller());
 
 // 在端口3000监听:
 app.listen(3000);
--- js_notes/hello-koa/controller.js
+++ js_notes/hello-koa/controller.js
@@ -7,6 +7,7 @@
 
 const fs = require('fs');
 
+// add url-route in /controllers:
 function addMapping(router, mapping) {
     for (let url in mapping) {
         if (mapping.hasOwnProperty(url) && url.startsWith('GET ')) {
@@ -17,31 +18,34 @@ function addMapping(router, mapping) {
             let path = url.substring(5);
             router.post(path, mapping[url]);
             console.log(`register URL mapping: POST ${path}`);
+        } else if (url.startsWith('PUT ')) {
+            let path = url.substring(4);
+            router.put(path, mapping[url]);
+            console.log(`register URL mapping: PUT ${path}`);
+        } else if (url.startsWith('DELETE ')) {
+            let path = url.substring(7);
+            router.put(path, mapping[url]);
+            console.log(`register URL mapping: DELETE ${path}`);
         } else {
             console.log(`invalid URL: ${url}`);
         }
     }
 }
 
-function addControllers(router) {
-    console.log(__dirname);
-    let files = fs.readdirSync(__dirname + '/controllers');
-    let js_files = files.filter((f) => {
+function addControllers(router, dir) {
+    fs.readdirSync(__dirname + '/' + dir).filter((f) => {
         return f.endsWith('.js');
-    });
-
-    for (let f of js_files) {
+    }).forEach((f) => {
         console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controllers/' + f);
+        let mapping = require(__dirname + '/' + dir + '/' + f);
         addMapping(router, mapping);
-    }
+    });
 }
 
 module.exports = function (dir) {
     let
         controllers_dir = dir || 'controllers',
         router = require('koa-router')();
-
     addControllers(router, controllers_dir);
 
     return router.routes();
